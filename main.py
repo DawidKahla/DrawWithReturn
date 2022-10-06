a64de0b704b5313dcb9d089a8cd28cc5afb7cd63
DawidKahla <dawid.kahla@onet.pl>
2022-10-06 15:03:03 +0200

    Made general method

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import itertools
+
 numbers = list(range(20, 36))
 cases = 0
 total_cases = 0
@@ -12,3 +14,16 @@ probability = cases/total_cases
 print('Combinations:', total_cases)
 print('Desired combinations:', cases)
 print('Probability:', probability)
+
+cases2 = 0
+total_cases2 = 0
+for set in itertools.product(numbers, repeat=4):
+    total_cases2 += 1
+    if set[0] + set[1] + set[2] + set[3] < 100:
+        cases2 += 1
+print (cases2)
+print (total_cases2)
+print (cases2/total_cases2)
+
+if cases == cases2 and total_cases == total_cases2:
+    print('It works')
